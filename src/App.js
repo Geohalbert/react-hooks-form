e14b9e16164985b928d07e4ca361673ee3105c4b
Hal Peters <geohalbert@gmail.com>
2020-10-24 16:01:41 -0500

    welcome portion started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { ReactComponent as GiantRobotLTD_Logo } from "./assets/GiantRobotLTD_Logo.svg";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <div className="Welcome" />
+      <div className="Welcome">
+        <div className="robotLogo">
+          <GiantRobotLTD_Logo />
+        </div>
+
+        <h1>Welcome</h1>
+
+        <h4>Please tell us a bit about yourself to get started.</h4>
+      </div>
       <form />
     </div>
   );
