459201ffaa371b82291f30d2ac5b2acb1b575a84
Hal Peters <geohalbert@gmail.com>
2020-10-24 16:15:20 -0500

    basic form setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import { ReactComponent as GiantRobotLTD_Logo } from "./assets/GiantRobotLTD_Logo.svg";
 import "./App.css";
 
 export default function App() {
+  // hooks used for form values
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+
   return (
     <div className="App">
       <div className="Welcome">
@@ -13,7 +20,37 @@ export default function App() {
 
         <h4>Please tell us a bit about yourself to get started.</h4>
       </div>
-      <form />
+      <form>
+        {/*  will create component for inputs for future styling */}
+        <label>FIRST NAME</label>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={e => setFirstName(e.target.value)}
+        />
+        <label>LAST NAME</label>
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={e => setLastName(e.target.value)}
+        />
+        <label>ADDRESS</label>
+        <input
+          type="text"
+          name="address1"
+          value={address1}
+          onChange={e => setAddress1(e.target.value)}
+        />
+        <label>ADDRESS 2 (OPTIONAL)</label>
+        <input
+          type="text"
+          name="address2"
+          value={address2}
+          onChange={e => setAddress2(e.target.value)}
+        />
+      </form>
     </div>
   );
 }
