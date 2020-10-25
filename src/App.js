509a1875534f0be878db9f7a0f874ae6c7ac6cf5
Hal Peters <geohalbert@gmail.com>
2020-10-25 07:23:12 -0500

    updated app layout and css

--- src/App.js
+++ src/App.js
@@ -1,55 +1,77 @@
 import { useState } from "react";
 import { ReactComponent as GiantRobotLTD_Logo } from "./assets/GiantRobotLTD_Logo.svg";
+import { ReactComponent as White_Arrow } from "./assets/White_Arrow.svg";
+import InputField from "./components/InputField";
 import "./App.css";
 
 export default function App() {
-  // hooks used for form values
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [address1, setAddress1] = useState("");
   const [address2, setAddress2] = useState("");
 
+  const isDisabled = !firstName || !lastName || !address1;
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    alert(`Submitting`);
+  };
   return (
     <div className="App">
-      <div className="Welcome">
+      <div className="greeting">
         <div className="robotLogo">
           <GiantRobotLTD_Logo />
         </div>
-
-        <h1>Welcome</h1>
-
-        <h4>Please tell us a bit about yourself to get started.</h4>
+        <div className="welcome">Welcome</div>
+        <div className="subText">
+          Please tell us a bit about yourself to get started.
+        </div>
       </div>
-      <form>
-        {/*  will create component for inputs for future styling */}
-        <label>FIRST NAME</label>
-        <input
+      <form onSubmit={handleSubmit}>
+        <InputField
+          autoFocus
+          required
           type="text"
+          label="FIRST NAME"
           name="firstName"
+          minlength="2"
           value={firstName}
+          pattern="([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
           onChange={e => setFirstName(e.target.value)}
         />
-        <label>LAST NAME</label>
-        <input
+
+        <InputField
+          required
           type="text"
+          label="LAST NAME"
           name="lastName"
+          minlength="2"
           value={lastName}
+          pattern="([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+"
           onChange={e => setLastName(e.target.value)}
         />
-        <label>ADDRESS</label>
-        <input
+
+        <InputField
+          required
           type="text"
+          label="ADDRESS"
           name="address1"
           value={address1}
+          minlength="5"
+          pattern="^[A-Za-z0-9'\.\-\s\,]"
           onChange={e => setAddress1(e.target.value)}
         />
-        <label>ADDRESS 2 (OPTIONAL)</label>
-        <input
+        <InputField
           type="text"
+          label="ADDRESS 2 (OPTIONAL)"
           name="address2"
           value={address2}
+          pattern="^[A-Za-z0-9'\.\-\s\,]"
           onChange={e => setAddress2(e.target.value)}
         />
+
+        <button type="submit" name="Submit" disabled={isDisabled}>
+          Next <White_Arrow />
+        </button>
       </form>
     </div>
   );
